af6b95cc75cd16bc1c0549f51d30af392544cfb4
Sven Schaefer <sven.schaefer@avl.com>
2020-02-13 14:53:10 +0100

    initial project setup

---
+++ src/test/java/com/avl/ditest/example/ExampleMultipleRepositoryDeployTest.java
@@ -0,0 +1,14 @@
+package com.avl.ditest.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class ExampleMultipleRepositoryDeployTest {
+ 
+	@Test
+    public void sampleMethod() {
+      fail("Implement this Test");
+    }
+ 
+}
